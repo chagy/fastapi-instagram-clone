9e1042a6db9621b15e07e388ef5c56a01350f3c7
chagy <chagy_x_@hotmail.com>
2022-03-10 08:47:18 +0700

    84

--- db/db_user.py
+++ db/db_user.py
@@ -1,3 +1,4 @@
+from db.hashing import Hash
 from db.models import DbUser
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
@@ -6,7 +7,7 @@ def create_user(db: Session,request: UserBase):
     new_user = DbUser(
         username = request.username,
         email = request.email,
-        password = request.password
+        password = Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
